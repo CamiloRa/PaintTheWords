7dd1f3cbeccc9daf963301e34f3f00c87c61a55b
Camilo Ramirez <camilor@gmail.com>
2018-12-31 21:58:50 -0500

    Finally a function

--- PaintTheWords.R
+++ PaintTheWords.R
@@ -1,11 +1,15 @@
 #PaintTheWords
 # function that takes one parameter, and makes the JPEGs from the Corpus after it has been transformed
 
-jpeg(filename=picture_name, picture_width, picture_height, units = "px")
+PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
+
+  jpeg(filename=picture_name, picture_width, picture_height, units = "px")
   wordcloud(ld_comments,
             scale=c(3.5,.10),
             max.words = 500,
             random.order = FALSE,
             rot.per = 0,
             colors= pal)
-dev.off()
+  dev.off()
+}
+
