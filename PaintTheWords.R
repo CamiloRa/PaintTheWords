c25c3d15cab372b043200001abb0818106001e35
CamiloRa <camilor@gmail.com>
2019-01-02 20:41:59 -0500

    Update PaintTheWords.R
    Small words

--- PaintTheWords.R
+++ PaintTheWords.R
@@ -1,12 +1,12 @@
 #PaintTheWords
 # function that takes one parameter, and makes the JPEGs from the Corpus after it has been transformed
 
-PaintTheWords <- function(picture_name, picture_width = 1200, picture_height = 800) {
+PaintTheWords <- function(picture_name, picture_width = 1280, picture_height = 800) {
 
   jpeg(filename=picture_name, picture_width, picture_height, units = "px")
   wordcloud(ld_comments,
             scale=c(3.5,.10),
-            max.words = 500,
+            max.words = 50,
             random.order = FALSE,
             rot.per = 0,
             colors= pal)
